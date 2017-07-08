ecbbed0bda9469278c38278729626f35240a60c4
ladamesny <adames.larry@gmail.com>
2017-07-08 19:13:44 -0400

    finished exercise 2

---
+++ getting_started/exercise_2.rb
@@ -0,0 +1,7 @@
+number = 1234
+
+puts "Number: #{number}"
+puts "Thousands Place: #{number/1000}"
+puts "Hundreds Place: #{number/100%10}"
+puts "Tens Place: #{number/10%10}"
+puts "Ones Place: #{number%10}"
