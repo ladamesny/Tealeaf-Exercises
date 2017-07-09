d2753ecca86ef5aab34ffcceb120242bdc72fe57
ladamesny <adames.larry@gmail.com>
2017-07-08 20:43:43 -0400

    solved exercise 6 variables

---
+++ variables/exercise_6.rb
@@ -0,0 +1,4 @@
+The error tells me that we tried to access a variable named 'shoes' within a scope
+where it was not defined. I am guessing it's a variable, and not a method, because 
+the error says the variable or method is undefined for "main:Object" instead of a
+an instance of a defined class.
